ec4bdbd124d14cab5e5a80c0e6a322bede13f016
Amit <amit.p@encoders.co.in>
2017-08-18 17:22:34 +0530

    Initial commit

---
+++ app/src/main/java/com/amittpad/cbnwf/cbnwf/MainActivity.java
@@ -0,0 +1,17 @@
+package com.amittpad.cbnwf.cbnwf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
